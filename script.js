5de185b4da06cc2af884bb8534605ecb0c5e4d00
York <sc17yxm@leeds.ac.uk>
2024-07-10 12:10:14 +0100

    add colouring effect to divs

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const sketchPad = document.querySelector(".main-container");
 
+function addHoverEffect (node) {
+    node.addEventListener("mouseover", addBackgroundColor);
+}
+
+function addBackgroundColor (node) {
+    if (!node.target.style["background-color"]) {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        node.target.style["background-color"] = `rgb(${red},${green},${blue})`;
+        node.target.style["opacity"] = "0.1";
+    }
+    let alpha = Number(node.target.style["opacity"]);
+    if (alpha < 1) {
+        alpha += 0.1;
+        node.target.style["opacity"]  = `${alpha}`;
+    }
+    
+}
+
 function addDivs (container, quantity) {
-    for (let i = 0; i < quantity; i++) {
+    let node;
+    for (let i = 0; i < quantity**2; i++) {
         node = document.createElement("div");
+        updateNodeStyle(node,quantity);
+        addHoverEffect(node);
         container.appendChild(node);
     }
 }
 
-function updateContainerStyle (sketchPad,size) {
-    for (let node of sketchPad.children) {
-        node.style.flex = `1 0 ${1 / size * 100}%`;
-    }
+function updateNodeStyle (node,size) {
+    node.style.cssText = `flex: 1 0 ${1 / size * 100}%`;    
+
 }   
 
 function clearContainer (sketchPad) {
@@ -19,9 +41,7 @@ function clearContainer (sketchPad) {
 
 function populatePage (sketchPad,quantity) {
 
-    addDivs(sketchPad,quantity**2);
-
-    updateContainerStyle(sketchPad,quantity);
+    addDivs(sketchPad,quantity);
 
 }
 
