dfb18697ad9f155c53c490697c65cbbc4c966cd4
York <sc17yxm@leeds.ac.uk>
2024-07-10 12:35:43 +0100

    add button to reset sketch pad

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const sketchPad = document.querySelector(".main-container");
 
-function addHoverEffect (node) {
-    node.addEventListener("mouseover", addBackgroundColor);
-}
+const resetBtn = document.querySelector("#reset");
+
+sketchPad.addEventListener("mouseover", (e) => {
+    addBackgroundColor(e.target);
+});
+
+resetBtn.addEventListener("click", () => {
+    const size = Number(prompt("Enter number of pixels:"));
+    populatePage(sketchPad,size);
+});
 
 function addBackgroundColor (node) {
-    if (!node.target.style["background-color"]) {
+    if (!node.style["background-color"]) {
         const red = Math.floor(Math.random() * 256);
         const green = Math.floor(Math.random() * 256);
         const blue = Math.floor(Math.random() * 256);
-        node.target.style["background-color"] = `rgb(${red},${green},${blue})`;
-        node.target.style["opacity"] = "0.1";
+        node.style["background-color"] = `rgb(${red},${green},${blue})`;
+        node.style["opacity"] = "0.1";
     }
-    let alpha = Number(node.target.style["opacity"]);
+    let alpha = Number(node.style["opacity"]);
     if (alpha < 1) {
         alpha += 0.1;
-        node.target.style["opacity"]  = `${alpha}`;
+        node.style["opacity"]  = `${alpha}`;
     }
     
 }
@@ -25,14 +32,12 @@ function addDivs (container, quantity) {
     for (let i = 0; i < quantity**2; i++) {
         node = document.createElement("div");
         updateNodeStyle(node,quantity);
-        addHoverEffect(node);
         container.appendChild(node);
     }
 }
 
 function updateNodeStyle (node,size) {
     node.style.cssText = `flex: 1 0 ${1 / size * 100}%`;    
-
 }   
 
 function clearContainer (sketchPad) {
@@ -40,9 +45,8 @@ function clearContainer (sketchPad) {
 }
 
 function populatePage (sketchPad,quantity) {
-
+    clearContainer(sketchPad);
     addDivs(sketchPad,quantity);
-
 }
 
-populatePage(sketchPad,32);
+addDivs(sketchPad,16);
